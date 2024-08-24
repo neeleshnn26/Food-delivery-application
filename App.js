e9b6de7d269c8cefcd9908db394a190971b72a60
Neelesh Nainwal <neeleshnainwal2001@gmail.com>
2024-08-24 23:40:00 +0530

    episode_04

--- App.js
+++ App.js
@@ -1,15 +1,519 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => JS object => HTML element
-// const heading= React.createElement("h1",{},"Hello from react");
+// ○ Think of a cool name for your app
+// ○ Build a AppLayout
+// ○ Build a Header Component with Logo & Nav Items & Cart
+// ○ Build a Body Component
+// ○ Build RestaurantList Component
+// ○ Build RestaurantCard Component
+// ○ Use static data initially
+// ○ Make your card dynamic(pass in props)
+// ○ Props - passing arguments to a function - Use Destructuring & Spread
+// operator
+// ○ Render your cards with dynamic data of restaurants
+// ○ Use Array.map to render all the restaurants
+
+const Header=()=>{
+    return(
+        <div className="header">
+            <div>
+                <img className="logo" src="https://static.vecteezy.com/system/resources/previews/004/312/696/original/food-logo-template-design-icon-illustration-vector.jpg" alt="Logo"/>
+            </div>
+            <div className="list">
+            <ul >
+                <li>Home</li>
+                <li>Contact us</li>
+                <li>About us</li>
+                <li>Cart</li>
+            </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard=(props)=>{
+    const{resData}=props
+    const{name,cuisines,costForTwo,cloudinaryImageId,avgRating}=resData?.info
+    return(
+        <div className="card">
+             <img className="card_img" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+ cloudinaryImageId }alt=""/>
+
+            <div className="details">
+              <h1>{name}</h1>
+              <h3>{cuisines .join(" ,")}</h3>
+              <h3>{costForTwo}</h3>
+              <h3>{avgRating}</h3>
+            </div>
+             
+        </div>
+    )
+}
+
+const resList=[
+    {
+      "info": {
+        "id": "676814",
+        "name": "Burgrill - The Win Win Burger",
+        "cloudinaryImageId": "f596863116da4b4688b48e813e25000d",
+        "locality": "Suddhowala",
+        "areaName": "Prem Nagar",
+        "costForTwo": "₹250 for two",
+        "cuisines": [
+          "Burgers",
+          "Healthy Food",
+          "American",
+          "Salads",
+          "Beverages",
+          "Desserts",
+          "Snacks"
+        ],
+        "avgRating": 4.5,
+        "parentId": "302366",
+        "avgRatingString": "4.5",
+        "totalRatingsString": "1K+",
+        "promoted": true,
+        "adTrackingId": "cid=16006568~p=0~adgrpid=16006568#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=676814~eid=67d47682-e5e8-44dc-945e-8da95727f4e6~srvts=1724521479663~collid=45995",
+        "sla": {
+          "deliveryTime": 22,
+          "lastMileTravel": 3.9,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20-25 mins",
+          "lastMileTravelString": "3.9 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-08-25 04:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "textExtendedBadges": {
+              
+            },
+            "textBased": {
+              
+            },
+            "imageBased": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "ITEMS",
+          "subHeader": "AT ₹129"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        },
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "--"
+          }
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "swiggy://menu?restaurant_id=676814",
+        "text": "RESTAURANT_MENU",
+        "type": "DEEPLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "311806",
+        "name": "Burger King",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/cab874d5-c7ed-4122-9eb9-935992f4bcee_311806.JPG",
+        "locality": "Ballupur",
+        "areaName": "Ballupur",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Burgers",
+          "American"
+        ],
+        "avgRating": 4.4,
+        "parentId": "166",
+        "avgRatingString": "4.4",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 34,
+          "lastMileTravel": 6.8,
+          "serviceability": "SERVICEABLE",
+          "slaString": "30-35 mins",
+          "lastMileTravelString": "6.8 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-08-25 05:00:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "Rxawards/_CATEGORY-Burger.png",
+              "description": "Delivery!"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "textBased": {
+              
+            },
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "imageId": "Rxawards/_CATEGORY-Burger.png",
+                    "description": "Delivery!"
+                  }
+                }
+              ]
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "ITEMS",
+          "subHeader": "AT ₹99"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        },
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "4.6",
+            "ratingCount": "500+"
+          },
+          "source": "GOOGLE",
+          "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "swiggy://menu?restaurant_id=311806",
+        "text": "RESTAURANT_MENU",
+        "type": "DEEPLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "69769",
+        "name": "Aman Vegetarian",
+        "cloudinaryImageId": "ygczg6efdbjietl1req7",
+        "locality": "Paltan Bazar",
+        "areaName": "Paltan Bazaar",
+        "costForTwo": "₹260 for two",
+        "cuisines": [
+          "North Indian",
+          "Thalis",
+          "Chinese"
+        ],
+        "avgRating": 4.3,
+        "veg": true,
+        "parentId": "13687",
+        "avgRatingString": "4.3",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 33,
+          "lastMileTravel": 8.8,
+          "serviceability": "SERVICEABLE",
+          "slaString": "30-35 mins",
+          "lastMileTravelString": "8.8 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-08-24 23:59:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "Rxawards/_CATEGORY-Dal%20Makhani.png",
+              "description": "Delivery!"
+            },
+            {
+              "imageId": "v1695133679/badges/Pure_Veg111.png",
+              "description": "pureveg"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "imageId": "Rxawards/_CATEGORY-Dal%20Makhani.png",
+                    "description": "Delivery!"
+                  }
+                },
+                {
+                  "attributes": {
+                    "imageId": "v1695133679/badges/Pure_Veg111.png",
+                    "description": "pureveg"
+                  }
+                }
+              ]
+            },
+            "textExtendedBadges": {
+              
+            },
+            "textBased": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹125 OFF",
+          "subHeader": "ABOVE ₹249",
+          "discountTag": "FLAT DEAL"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        },
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "3.9",
+            "ratingCount": "100+"
+          },
+          "source": "GOOGLE",
+          "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "swiggy://menu?restaurant_id=69769",
+        "text": "RESTAURANT_MENU",
+        "type": "DEEPLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "74373",
+        "name": "Walk In Woods",
+        "cloudinaryImageId": "rx6kpst6wxyw9vvj7wo8",
+        "locality": "General Mahadev Singh Road",
+        "areaName": "Ballupur",
+        "costForTwo": "₹450 for two",
+        "cuisines": [
+          "North Indian",
+          "Chinese",
+          "Biryani"
+        ],
+        "avgRating": 4.3,
+        "parentId": "7148",
+        "avgRatingString": "4.3",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 32,
+          "lastMileTravel": 6.5,
+          "serviceability": "SERVICEABLE",
+          "slaString": "30-35 mins",
+          "lastMileTravelString": "6.5 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-08-25 00:00:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "Rxawards/_CATEGORY-North%20Indian.png",
+              "description": "Delivery!"
+            },
+            {
+              "imageId": "Rxawards/_CATEGORY-Snacks.png",
+              "description": "Delivery!"
+            },
+            {
+              "imageId": "Rxawards/_CATEGORY-Thali.png",
+              "description": "Delivery!"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "textExtendedBadges": {
+              
+            },
+            "textBased": {
+              
+            },
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "imageId": "Rxawards/_CATEGORY-North%20Indian.png",
+                    "description": "Delivery!"
+                  }
+                },
+                {
+                  "attributes": {
+                    "imageId": "Rxawards/_CATEGORY-Snacks.png",
+                    "description": "Delivery!"
+                  }
+                },
+                {
+                  "attributes": {
+                    "imageId": "Rxawards/_CATEGORY-Thali.png",
+                    "description": "Delivery!"
+                  }
+                }
+              ]
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹100 OFF",
+          "subHeader": "ABOVE ₹199",
+          "discountTag": "FLAT DEAL"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        },
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "--"
+          }
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "swiggy://menu?restaurant_id=74373",
+        "text": "RESTAURANT_MENU",
+        "type": "DEEPLINK"
+      }
+    }]
+
+const Body=()=>{
+    return (
+        <div className="body">
+            <div className="top">
+                <div>
+                <input className="type" type="text" placeholder="Search for food"/>
+                <button className="search"> Search</button>
+                </div>
+
+                <button className="btn">Top rated restaurants</button>
+                
+            </div>
+
+            <div className="res-container">
+                {
+                    resList.map((restaurant)=>
+                        <RestaurantCard  key={restaurant.info.id} resData={restaurant}/>
+                    )
+                }
+                  
+            </div>
 
 
-// ---------------------Writing JSX--------------------------------
-// JSX => Babel transpiles it to react.createElement => React.createElement ->(JS object) => HTML Element (on render)
+        </div>
+    )
+}
 
-const jsxHeading= <h1 className="jsx">Hello from JSX heading</h1>
 
 
+
+const AppLayout=()=>{
+    return(
+        <div className="AppLayout">
+          <Header/>
+          <Body/>
+        </div>
+    )
+
+}
 const root= ReactDOM.createRoot(document.getElementById("root"))
-root.render(jsxHeading);
+root.render(<AppLayout/>);
